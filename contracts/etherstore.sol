97501d54913a4249e1884867e43162f66272d4e1
Bernhard Mueller <b-mueller@users.noreply.github.com>
2018-11-07 09:27:43 +0700

    Update etherstore.sol

--- contracts/etherstore.sol
+++ contracts/etherstore.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.24;
 
 contract EtherStore {
 
+    // Add a comment
+
     uint256 public withdrawalLimit = 1 ether;
     mapping(address => uint256) public lastWithdrawTime;
     mapping(address => uint256) public balances;
@@ -11,7 +13,6 @@ contract EtherStore {
     }
 
     function withdrawFunds (uint256 _weiToWithdraw) public {
-        require(balances[msg.sender] >= _weiToWithdraw);
         // limit the withdrawal
         require(_weiToWithdraw <= withdrawalLimit);
         // limit the time allowed to withdraw
