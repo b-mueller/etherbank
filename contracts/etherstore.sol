9d330a34e7d204a36cb77103750dc3e2a8e1cba4
Bernhard Mueller <b-mueller@users.noreply.github.com>
2018-11-07 09:26:19 +0700

    Update etherstore.sol

--- contracts/etherstore.sol
+++ contracts/etherstore.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.24;
 
 contract EtherStore {
 
+    // Add a comment
+
     uint256 public withdrawalLimit = 1 ether;
     mapping(address => uint256) public lastWithdrawTime;
     mapping(address => uint256) public balances;
